d382630e302fb37b7b40fe47eda50b871910691e
pandeypriti015 <pandeypriti015@gmail.com>
2019-11-22 16:32:48 +0530

    seerializer views

--- src/MyntraApp/serializers.py
+++ src/MyntraApp/serializers.py
@@ -1,13 +1,20 @@
 from .models import *
 from rest_framework import serializers
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
+class CatergoryNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name','get_image',)
 
 class ItemSerializer(serializers.ModelSerializer):
+    category_name = CatergoryNameSerializer()
+    image_url = serializers.CharField(source='get_image')
     class Meta:
         model = Item
         fields = '__all__'
